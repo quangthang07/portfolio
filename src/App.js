768249db9afc231a50b840d236e949b43ce5cade
quangthang07 <nguyenquangthang1703@gmail.com>
2023-04-09 03:22:14 +0900

    update app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import './App.scss';
-import Layout from './components/layout';
 import {Routes, Route} from 'react-router-dom';
-import Home from './components/home'
-import About from './components/about';
-import Skills from './components/skills';
-import Resume from './components/resume';
-import Work from './components/work'
-import Contact from './components/contact';
-import React from 'react';
+import React, { Suspense } from 'react';
+const Layout = React.lazy(() => import('./components/layout'));
 
 
 function App() {
     return (
-        <>
+        <Suspense>
             <Routes>
                 <Route path='/portfolio' element={<Layout/>}>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/about' element={<About/>}/>
-                    <Route path='/skills' element={<Skills/>}/>
-                    <Route path='/resume' element={<Resume/>}/>
-                    <Route path='/work' element={<Work/>}/>
-                    <Route path='/contact' element={<Contact/>}/>
                 </Route>
             </Routes>
-        </>
+        </Suspense>
     );
 }
 
